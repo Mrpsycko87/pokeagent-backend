5a2138ef64ed36d3dba9ce06233fab51fc43bfa5
Pablo Morales <pablomorgithub@gmail.com>
2025-03-09 07:55:09 -0500

    vercel cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://pokeagent-frontend.onrender.com', // Permite solo peticiones desde Angular
+    origin: 'https://pokeagent-frontend.vercel.app/', // Permite solo peticiones desde Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // Permitir envío de cookies si es necesario
